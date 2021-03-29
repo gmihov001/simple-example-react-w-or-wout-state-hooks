83bae4006c53c0a98e26620fb59cace68e7f632d
George Mihov <gmihov001@gmail.com>
2021-03-29 12:58:37 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 // example of the difference between a state variable (hook variable in functions) and a simple function variable
-export default function App() {
+export function Home() {
 	var number = 12;
 	const [numberHook, setNumberHook] = useState(12);
 
 	return (
-		<div className="App">
-			<h1>Hello CodeSandbox</h1>
-			<h2>Start editing to see some magic happen!</h2>
+		<div className="text-center">
+			<h2>Example of state vs non-state (hook vs non-hook) behavior</h2>
+			<br />
+			<br />
 			This is my number: <h1>{numberHook}</h1>
 			{/* if using simple variable 'number' the page does not re-render; switching to the hook variable 'numberHook' causes re-render and the displayed number updates */}
 			<br />
 			<br />
 			<br />
 			<button
+				className="btn btn-outline-success btn-lg"
 				onClick={() => {
 					number = 13;
 					setNumberHook(16);
