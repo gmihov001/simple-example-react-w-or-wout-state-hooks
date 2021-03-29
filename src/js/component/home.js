04d1b55aa6e606864c1a5371fa0c04d8cb74212c
George Mihov <gmihov001@gmail.com>
2021-03-29 12:51:18 +0000

    Updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// example of the difference between a state variable (hook variable in functions) and a simple function variable
+export default function App() {
+	var number = 12;
+	const [numberHook, setNumberHook] = useState(12);
 
-//create your first component
-export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App">
+			<h1>Hello CodeSandbox</h1>
+			<h2>Start editing to see some magic happen!</h2>
+			This is my number: <h1>{numberHook}</h1>
+			{/* if using simple variable 'number' the page does not re-render; switching to the hook variable 'numberHook' causes re-render and the displayed number updates */}
+			<br />
+			<br />
+			<br />
+			<button
+				onClick={() => {
+					number = 13;
+					setNumberHook(16);
+					console.log(number);
+				}}>
+				Change number
+			</button>
 		</div>
 	);
 }
